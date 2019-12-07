b33c5cc0eb166680dddb46c508e1948956109163
aditya <nayakadi777@gmail.com>
2019-12-07 23:36:05 +0530

    Updates

---
+++ precache-manifest.7219eff30bfa54f8b25bf28cd4673fa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d13e4e52eb0fbb685d5690ba244eb64",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "557513e36d9698007a35",
+    "url": "/monster-rolodex/static/css/main.c9934fc7.chunk.css"
+  },
+  {
+    "revision": "934c4dc1d7fa9ce4f7ad",
+    "url": "/monster-rolodex/static/js/2.4f80c56b.chunk.js"
+  },
+  {
+    "revision": "557513e36d9698007a35",
+    "url": "/monster-rolodex/static/js/main.5e2eb0da.chunk.js"
+  },
+  {
+    "revision": "5bed4f6902bce2a9e0b9",
+    "url": "/monster-rolodex/static/js/runtime-main.f1ce22d3.js"
+  }
+]);
